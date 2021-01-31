a2d70bc22f860336245d615d7957ccbbef320b06
Dias <63576075+dias000@users.noreply.github.com>
2021-01-31 17:55:30 +0600

    Update python finding Angle MBC.py

--- python finding Angle MBC.py
+++ python finding Angle MBC.py
@@ -3,6 +3,6 @@ from math import degrees, atan2
 AB = int(input())
 BC = int(input())
 
-RESULT = round(degrees(atan2(AB, BC)))
-print(str(RESULT) + '°')
+res = round(degrees(atan2(AB, BC)))
+print(str(res) + '°')
 
